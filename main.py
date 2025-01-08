3d5d7a0ffa50e11aa2260b8ca6b28542850c49d3
Himanshu-Verma-git <work.himanshuverma@outlook.com>
2025-01-09 02:26:17 +0530

    pitch roll yaw added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from client import Client
 import calculation
+import body
 import time
 import struct
 import asyncio
@@ -9,15 +10,31 @@ import pyautogui
 
 pyautogui.PAUSE = 0.0
 
-async def print_data_g(Character, data: bytearray):
+accel_data:list = [0, 0, 0]
+gyro_data:list = [0, 0, 0]
+magnet_data:list = [0, 0, 0]
+
+kalman_roll = body.KalmanFilter()
+kalman_pitch = body.KalmanFilter()
+kalman_yaw = body.KalmanFilter()
+dt = 0.01
+
+kalman_filters = [kalman_roll, kalman_pitch, kalman_yaw]
+
+async def setData(Character, data: bytearray):
     data = getData(data)
-    print("GYRO",data)
-async def print_data_a(Character, data: bytearray):
-    data = getData(data)
-    print("ACC",data)
-async def print_data_m(Character, data: bytearray):
-    data = getData(data)
-    print("MAG",data)
+    if(Character.uuid == "00000011-0000-1000-8000-00805f9b34fb"): gyro_data[0] = data
+    if(Character.uuid == "00000012-0000-1000-8000-00805f9b34fb"): gyro_data[1] = data
+    if(Character.uuid == "00000013-0000-1000-8000-00805f9b34fb"): gyro_data[2] = data
+    if(Character.uuid == "00000021-0000-1000-8000-00805f9b34fb"): accel_data[0] = data
+    if(Character.uuid == "00000022-0000-1000-8000-00805f9b34fb"): accel_data[1] = data
+    if(Character.uuid == "00000023-0000-1000-8000-00805f9b34fb"): accel_data[2] = data
+    if(Character.uuid == "00000031-0000-1000-8000-00805f9b34fb"): magnet_data[0] = data
+    if(Character.uuid == "00000032-0000-1000-8000-00805f9b34fb"): magnet_data[1] = data
+    if(Character.uuid == "00000033-0000-1000-8000-00805f9b34fb"): magnet_data[2] = data
+# async def printdata(Character, data: bytearray):
+#     print(Character.uuid)
+#     print(type(Character))
 
 def getData(byte_array, byte_order='little'):
     return struct.unpack('f', byte_array)[0]
@@ -81,23 +98,25 @@ async def main():
     await client.servAndChar()
     print("-------------------------------------------------------------------------------------------")
 
-    await client.writeToChar(CHAR_FLAG, b'\x01')
     try:
-        await client.start_notification(CHAR_GX, handler=print_data_g)
-        await client.start_notification(CHAR_GY, handler=print_data_g)
-        await client.start_notification(CHAR_GZ, handler=print_data_g)
-        await client.start_notification(CHAR_AX, handler=print_data_a)
-        await client.start_notification(CHAR_AY, handler=print_data_a)
-        await client.start_notification(CHAR_AZ, handler=print_data_a)
-        await client.start_notification(CHAR_MX, handler=print_data_m)
-        await client.start_notification(CHAR_MY, handler=print_data_m)
-        await client.start_notification(CHAR_MZ, handler=print_data_m)
+        await client.start_notification(CHAR_GX, handler=setData)
+        await client.start_notification(CHAR_GY, handler=setData)
+        await client.start_notification(CHAR_GZ, handler=setData)
+        await client.start_notification(CHAR_AX, handler=setData)
+        await client.start_notification(CHAR_AY, handler=setData)
+        await client.start_notification(CHAR_AZ, handler=setData)
+        await client.start_notification(CHAR_MX, handler=setData)
+        await client.start_notification(CHAR_MY, handler=setData)
+        await client.start_notification(CHAR_MZ, handler=setData)
     except Exception as e:print(e)
     print("Conformation Sent.")
 
+    await client.writeToChar(CHAR_FLAG, b'\x01')
     while(client.connected):
-        # print("Keep ALive")
-        await asyncio.sleep(1)
+        pitch, roll, yaw = body.calculate_orientation(accel_data, gyro_data, magnet_data, 0, kalman_filters)
+        # print("PRY")
+        print(pitch, "\t", roll, "\t", yaw, "\t")
+        await asyncio.sleep(0.1)
 
     # await client.stop_notification(char_uuid=CHAR_AX)
     # await client.stop_notification(char_uuid=CHAR_AY)
